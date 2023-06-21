b6b86fea58f89e7949b47ddc85438a23f75aab0f
Helmer Den Dekker <Helmer.DenDekker@samenlevingszaken.onmicrosoft.com>
2023-06-21 16:59:22 +0200

    Money

---
+++ HelmerDemo.WebShop.Domain/Shared/Money.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelmerDemo.WebShop.Domain.Shared
+{
+    public class Money
+    {
+        /// <summary>
+        /// Gets the currency
+        /// </summary>
+        public string Currency { get; private set; }
+
+        /// <summary>
+        /// Gets the amount
+        /// </summary>
+        public decimal Amount { get; private set; }
+    }
+}
